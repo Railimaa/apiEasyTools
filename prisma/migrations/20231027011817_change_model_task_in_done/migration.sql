11df3490b844f4fad089ef26e87de2b79d68aec8
Railimaa <ferreiralimarai0@gmail.com>
2023-10-26 22:19:49 -0300

    feat: change model task in column done

---
+++ prisma/migrations/20231027011817_change_model_task_in_done/migration.sql
@@ -0,0 +1,9 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `completed` on the `task` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "task" DROP COLUMN "completed",
+ADD COLUMN     "done" BOOLEAN NOT NULL DEFAULT false;
